c2924a3a2be473e775aaf193fee2fd87d32aba9c
HuYunXiu <huyunxiu01@gmail.com>
2026-05-17 10:27:49 +0800

    feat: add ultracite

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
+import ultracite from "ultracite/oxfmt";
+import core from "ultracite/oxlint/core";
+import react from "ultracite/oxlint/react";
+import remix from "ultracite/oxlint/remix";
+import vitest from "ultracite/oxlint/vitest";
 import { defineConfig } from "vite-plus";
 
 export default defineConfig({
-  staged: {
-    "*": "vp check --fix",
+  fmt: {
+    ...ultracite,
   },
-  fmt: {},
-  lint: { options: { typeAware: true, typeCheck: true } },
+  lint: {
+    extends: [core, react, remix, vitest],
+    options: { typeAware: true, typeCheck: true },
+  },
   run: {
     cache: false,
+  },
+  staged: {
+    "*": "vp check --fix",
   },
 });
